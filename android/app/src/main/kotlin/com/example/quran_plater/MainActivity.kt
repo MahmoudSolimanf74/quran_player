7d85cb391a9f74589b48cfbd677355f62a7e4725
MahmoudSolimanf74 <mahmoudsolimanf74@gmail.com>
2026-03-06 10:52:51 +0200

    main

---
+++ android/app/src/main/kotlin/com/example/quran_plater/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quran_plater
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=G:\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=G:\programing\Flutter\quran_player\quran_player"
+export "FLUTTER_APPLICATION_PATH=G:\programing\Flutter\quran_player\quran_plater"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
